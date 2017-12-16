c8b56574abc4faa91bd1b3c2970955d749b7421e
yurigm97 <yurigm97@hotmail.com>
2017-12-16 04:51:59 -0300

    mutation-types: Add mutation types for add-contact state module

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,4 +1,12 @@
+// Types of ContactList component
 export const RECEIVE_CONTACTS = 'RECEIVE_CONTACTS';
 export const CHANGE_IS_LOADING_DATA = 'CHANGE_IS_LOADING_DATA';
 export const CHANGE_FILTER_TAG = 'CHANGE_FILTER_TAG';
 export const INPUT_SEARCH_TEXT = 'INPUT_SEARCH_TEXT';
+
+// Types of AddContact component
+export const RECEIVE_CONTACTS_SOURCES = 'RECEIVE_CONTACTS_SOURCES';
+export const CHANGE_IS_SENDING_DATA = 'CHANGE_IS_SENDING_DATA';
+export const ADD_PHONE_NUMBER = 'ADD_PHONE_NUMBER';
+export const ADD_EMAIL_ADDRESS = 'ADD_EMAIL_ADDRESS';
+export const RESET_STATE = 'RESET_STATE';
