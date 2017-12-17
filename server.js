c6e6b0a55b01d05878da420b5dc2c28518fc8eb3
yurigm97 <yurigm97@hotmail.com>
2017-12-17 01:05:14 -0300

    server: Update change public path

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-app.use(express.static(path.join(__dirname, 'dist')));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
